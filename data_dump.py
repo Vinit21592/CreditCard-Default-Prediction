50c7e15a8ded4defae95b42d272f2c2ae04e5e52
Vinit21592 <vinit.londhe21@gmail.com>
2023-01-06 20:31:13 +0530

    This is the start of project

---
+++ data_dump.py
@@ -0,0 +1,22 @@
+import pymongo
+import pandas as pd
+import json
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb+srv://Vinit21592:vins21592@cluster0.dl9uoq9.mongodb.net/?retryWrites=true&w=majority")
+
+DATA_FILE_PATH="/config/workspace/UCI_Credit_Card.csv"
+DATABASE_NAME="credit"
+COLLECTION_NAME="creditdata"
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    # Convert dataframe to json so that we can dump these record in mongodb
+    df.reset_index(drop=True,inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    # insert converted json record to mongodb
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
