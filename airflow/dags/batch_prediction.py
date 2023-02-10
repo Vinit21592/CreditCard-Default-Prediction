7a5ec0a22b7d38aba9407b16bdbc040ba1905b6c
Vinit21592 <vinit.londhe21@gmail.com>
2023-02-10 22:01:47 +0530

    dags

--- airflow/dags/batch_prediction.py
+++ airflow/dags/batch_prediction.py
@@ -13,7 +13,7 @@ with DAG(
     # [END default_args]
     description='Credit Card Default Prediction',
     schedule_interval="@weekly",
-    start_date=pendulum.datetime(2023, 1, 10, tz="UTC"),
+    start_date=pendulum.datetime(2023, 2, 10, tz="UTC"),
     catchup=False,
     tags=['example'],
 ) as dag:
